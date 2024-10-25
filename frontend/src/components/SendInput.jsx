730e278f6106ab41bce6e4289044dc56945f8038
Ajay Chauhan <152907284+AJAY-CHAUHAN0001@users.noreply.github.com>
2024-10-25 17:48:09 +0530

    Update SendInput.jsx

--- frontend/src/components/SendInput.jsx
+++ frontend/src/components/SendInput.jsx
@@ -13,7 +13,7 @@ export const SendInput = () => {
   const sendInputHandler = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`http://localhost:8080/api/v1/message/send/${selectedUser?._id}`,{message},{
+      const res = await axios.post(`https://chat-app-backend-q4d3.onrender.com/api/v1/message/send/${selectedUser?._id}`,{message},{
         headers:{
           "Content-Type":"application/json"
         },
